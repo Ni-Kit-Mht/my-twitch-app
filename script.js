58a59dae9338d6fea6859a5b0a676f420a8df4e9
Nikit <nikitmht4@example.com>
2024-07-23 19:25:11 +0545

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-new Twitch.Embed("twitch-embed", {
-    width: 800,
-    height: 450,
-    channel: "smallgamedeveloper"
-});
+function startStream() {
+    const channelName = document.getElementById('channel-name').value;
+    if (channelName) {
+        const embed = document.getElementById('twitch-embed');
+        embed.innerHTML = ''; // Clear any existing embed
+        new Twitch.Embed("twitch-embed", {
+            width: "100%",
+            height: "100%",
+            channel: channelName
+        });
+    } else {
+        alert('Please enter a Twitch channel name.');
+    }
+}
